439936f0821b9a11d34f10916414e67aab3d03fd
Rishu Gupta <guptarishu04@gmail.com>
2020-02-01 11:45:50 +0530

    ask me anything

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,40 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Center(
+              child: Text('Ask Me Anything'),
+            ),
+            backgroundColor: Colors.blue[900],
+          ),
+          body: MyApp(),
+          backgroundColor: Colors.blueAccent,
+        ),
       ),
     );
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int name = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            name = Random().nextInt(5) + 1;
+          });
+        },
+        child: Image.asset('images/ball$name.png'),
+      ),
+    );
+  }
+}
